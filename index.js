36a2a037c0a570877730f65713133cc274aad504
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-07 22:30:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 // Code your solution here
 
-function  findMatching(drivers,string){
+function findMatching(drivers,nameTofind){
+    //drivers(array of strings)
+    // ["karson", "monica", "Fatima"]
 
-var drivers = string.toUpperCase === string.toLowecase;
+    //nameTofind (string)
+    //"Karson"
 
-return drivers
+    //console.log(drivers)
+    //console.log(nameTofind)
+    
+  return  drivers.filter(function(driver){
+        //driver = "karson"
+      return  driver===nameTofind 
+      
+        // === does the thing on the left equal the thing on the right?
+        // = this assigns a value to a variable (we use this with const and let)
 
+    })
+   
+
+}
+function fuzzyMatch(drivers,listName){
+
+    return  drivers.filter(function(driver){
+        
+      return  driver[0]===listName[0]
+
+
+})
+}
+
+function matchName(drivers,matchToFind ){
+    return  drivers.filter(function(driver){
+        
+        return drivers[driver] ===drivers[matchToFind]
+  
+})
 }
